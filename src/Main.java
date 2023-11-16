f698f1b507d3319e077cfd53e367644000219ba0
4121n <4121nnn@gmail.com>
2023-11-16 18:39:26 +0300

    adding generateIntArrAy method

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ import sort.BubbleSort;
 
 import java.util.Arrays;
 
+import sort.SortUtils;
+import sort.SortUtils.*;
+
 public class Main {
     public static void main(String[] args) {
 
             BubbleSort bubble = new BubbleSort();
-            Integer[] arr = new Integer[]{3,2,1,5,67,3,1};
+            Integer[] arr = SortUtils.generateIntArray(10);
         System.out.println(Arrays.toString(bubble.sort(arr)));
 
 

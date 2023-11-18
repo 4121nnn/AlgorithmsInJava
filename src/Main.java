e288b94db629197d7034725d10e1736a36f8d430
4121n <4121nnn@gmail.com>
2023-11-18 19:43:31 +0300

    started implementing merge sort(not finished)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
-import sort.BubbleSort;
+import sort.*;
 
 import java.util.Arrays;
 
-import sort.QuickSort;
-import sort.SortUtils;
 import sort.SortUtils.*;
-import sort.TestSort;
 
 public class Main {
     public static void main(String[] args) {
             long startTime = System.nanoTime();
 
             BubbleSort bubble = new BubbleSort();
+            SelectionSort selection = new SelectionSort();
             QuickSort quick = new QuickSort();
+
             //Integer[] arr = new Integer[]{4,5,3,7,2,9,1,0,8};
-            Integer[] arr = SortUtils.generateIntArray(10000);
+            Integer[] arr = SortUtils.generateIntArray(100000);
             quick.sort(arr);
             //System.out.println(Arrays.toString(arr));
-        //System.out.println(TestSort.isSorted(arr));
+            //System.out.println(TestSort.isSorted(arr));
 
         long time = (System.nanoTime() - startTime) / 1000000;
         System.out.println(time + " ms");
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class MergeSort implements Sort{
+
+   @Override
+    public <T extends Comparable<T>> T[] sort(T[] arr){
+       int n = arr.length;
+
+       return arr;
+    }
+
+    public <T extends Comparable<T>> void sort(T[] arr, int from, int to){
+       if(arr.length <= 1) return;
+       sort(arr, from, to/2);
+       sort(arr, to/2, to);
+
+    }
+
+}
+

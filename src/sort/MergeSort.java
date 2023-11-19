0702b7c4427b4ecd8bf3dc20f17a391bb36ed1a6
4121n <4121nnn@gmail.com>
2023-11-19 18:07:53 +0300

    started implementing merge sort(not finished) just little tired so i just commit and push it

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -10,7 +10,7 @@ public class MergeSort implements Sort{
     }
 
     public <T extends Comparable<T>> void sort(T[] arr, int from, int to){
-       if(arr.length <= 1) return;
+       if(to - from <= 1) return;
        sort(arr, from, to/2);
        sort(arr, to/2, to);
 

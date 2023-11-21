9bb12c184b06871dbef10b06ab25ebdab4b7c5ff
4121n <4121nnn@gmail.com>
2023-11-21 19:54:19 +0300

    implementing merge

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,22 +1,40 @@
 package sort;
 
+import java.util.Arrays;
+
 public class MergeSort implements Sort{
 
-   @Override
-    public <T extends Comparable<T>> T[] sort(T[] arr){
-       int n = arr.length;
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] arr) {
+        int n = arr.length;
+        if(n <= 1) return arr;
 
-       return arr;
+        T[] left  = Arrays.copyOfRange(arr,0, n/2);
+        T[] right = Arrays.copyOfRange(arr,n/2, n);
+
+        left  = sort(left);
+        right = sort(right);
+
+        merge(arr, left, right);
+
+        return arr;
     }
-
-    public <T extends Comparable<T>> void sort(T[] arr, int from, int to){
-       if(to - from <= 1) return;
-
-
-       sort(arr, from, to/2);
-       sort(arr, to/2, to);
-
+    private <T extends Comparable<T>> void merge(T[] arr, T[] left, T[] right){
+        int L = left.length, R = right.length;
+        int i = 0, j = 0, k = 0;
+        while(i < L && j < R){
+            if(left[i].compareTo(right[j]) < 0){
+                arr[k++] = left[i++];
+            }else {
+                arr[k++] = right[j++];
+            }
+        }
+        while(i < L){
+            arr[k++] = left[i++];
+        }
+        while(j < R){
+            arr[k++] = right[j++];
+        }
     }
-
 }
 
